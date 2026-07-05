4b65c96beb025f086764e023f562a2336cd6e05f
sukhoon912 <s912ss912@naver.com>
2026-07-05 20:36:13 +0900

    chore: 프로젝트 Tailwind CSS 초기 환경 세팅 완료

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // src 폴더 내부의 모든 컴포넌트 감지
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
